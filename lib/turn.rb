fe8580195d2600db527ab2a79e5c538dd234dd71
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-09-30 20:32:10 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,33 +1,29 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+def input_to_index(input)
+  input.to_i - 1
 end
 
-# code your input_to_index and move method here!
-
-def input_to_index(user_input)
-  user_input.to_i - 1
+#if index is valid...
+def valid_move?(number_entered, board)
+  number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
 end
-
-def move(board, index, current_player = "X")
-  board[index] = current_player
+#if index is valid...make the move for index
+def move(array, index, name = "X")
+  array[index] = name
 end
-
-def valid_move?(board,index_num)
-if index_num.between?(0,8) && !position_taken?(board,index_num)
-  return true
-else
-  false
-end
-end
-
+#if index is valid...show the board
 def position_taken?(board, answer)
   board[answer] != " "
 end
 
+def display_board(board)
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+#this method need to be developed ________________
 def turn(board)
     puts "Please enter 1-9:"
     answer = gets.chomp
