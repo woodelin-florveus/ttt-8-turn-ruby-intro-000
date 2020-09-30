a580444769e8148dd7b478b5537c1a05d896aef8
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-09-30 20:24:13 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,34 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def valid_move?(board,index_num)
+if index_num.between?(0,8) && !position_taken?(board,index_num)
+  return true
+else
+  false
+end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index_num)
+
+  end
+end
